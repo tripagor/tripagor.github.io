8ac61c3d7611b9b02552fa473d29c031f194d237
Jens Laufer <jenslaufer@gmail.com>
2016-02-22 11:30:20 +0100

    added code

---
+++ src/main/java/com/tripagor/util/extrator/App.java
@@ -0,0 +1,13 @@
+package com.tripagor.util.extrator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
