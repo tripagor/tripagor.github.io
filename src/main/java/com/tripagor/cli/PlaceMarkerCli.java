145017487a00aa6774cd1055d580748ed369fe2b
Jens Laufer <jenslaufer@gmail.com>
2016-03-09 15:58:03 +0100

    initial place marker

---
+++ src/main/java/com/tripagor/cli/PlaceMarkerCli.java
@@ -0,0 +1,67 @@
+package com.tripagor.cli;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+
+import com.tripagor.exporter.PlaceMarker;
+
+public class PlaceMarkerCli {
+
+	private static Options options;
+
+	public static void main(String[] args) {
+		PlaceMarker exporter = new PlaceMarker();
+
+		options = new Options();
+		options.addOption("d", true, "mongo uri");
+		options.addOption("c", true, "collection");
+		options.addOption("n", true, "maxium number of markers to be placed");
+		options.addOption("h", false, "this help");
+
+		CommandLineParser parser = new DefaultParser();
+		try {
+			CommandLine cmd = parser.parse(options, args);
+			if (cmd.hasOption("h")) {
+				help();
+			}
+
+			String uri = "";
+			int numberOfPlacesToAdd = 0;
+
+			if (cmd.hasOption("d")) {
+				uri = cmd.getOptionValue("d");
+			} else {
+				help();
+			}
+
+			String collection = "";
+			if (cmd.hasOption("c")) {
+				collection = cmd.getOptionValue("c");
+			} else {
+				help();
+			}
+
+			if (cmd.hasOption("n")) {
+				numberOfPlacesToAdd = Integer.parseInt(cmd.getOptionValue("n"));
+			}
+
+			if (numberOfPlacesToAdd > 0) {
+				exporter.setNumberOfPlacesToAdd(numberOfPlacesToAdd);
+			}
+			System.out.println("Adding places for " + uri);
+			exporter.doMark(uri, collection);
+		} catch (Exception e) {
+			System.err.println("An problem occured:" + e);
+		}
+	}
+
+	private static void help() {
+		HelpFormatter formater = new HelpFormatter();
+		formater.printHelp(PlaceMarkerCli.class.getName(), options);
+		System.exit(0);
+	}
+
+}
---
+++ src/main/java/com/tripagor/exporter/PlaceMarker.java
@@ -0,0 +1,60 @@
+package com.tripagor.exporter;
+
+import java.math.BigDecimal;
+import java.util.Locale;
+
+import org.bson.Document;
+
+import com.google.maps.GeocodingApi;
+import com.google.maps.PlacesApi;
+import com.google.maps.model.AddressType;
+import com.google.maps.model.GeocodingResult;
+import com.google.maps.model.LatLng;
+import com.google.maps.model.PlacesSearchResponse;
+import com.google.maps.model.PlacesSearchResult;
+import com.google.maps.model.RankBy;
+import com.mongodb.Block;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+
+public class PlaceMarker {
+
+	private int numberOfPlacesToAdd;
+
+	public void doMark(String uri, String collectionName) {
+
+		MongoClientURI mongoClientURI = new MongoClientURI(uri);
+		MongoClient mongoClient = new MongoClient(mongoClientURI);
+		final MongoCollection<Document> collection = mongoClient.getDatabase(mongoClientURI.getDatabase())
+				.getCollection(collectionName);
+
+		FindIterable<Document> iterable = collection.find(new Document("is_evaluated", new Document("$exists", false)))
+				.limit(numberOfPlacesToAdd).sort(new Document("booking_com_id", -1));
+
+		try {
+
+			iterable.forEach(new Block<Document>() {
+
+				public void apply(Document document) {
+					try {
+						System.out.println(document.get("name"));
+					} catch (Exception e) {
+					}
+				}
+			});
+
+		} catch (Exception e) {
+			throw new RuntimeException("failed " + e, e);
+		} finally {
+			mongoClient.close();
+		}
+
+	}
+
+	public void setNumberOfPlacesToAdd(int numberOfPlacesToAdd) {
+		this.numberOfPlacesToAdd = numberOfPlacesToAdd;
+	}
+
+}
