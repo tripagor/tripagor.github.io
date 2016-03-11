0e664e83b419b7d1cee733793023c36db9dfecd4
Jens Laufer <jenslaufer@gmail.com>
2016-03-11 15:05:42 +0100

    added more attributes

--- src/main/java/com/tripagor/hotels/model/Hotel.java
+++ src/main/java/com/tripagor/hotels/model/Hotel.java
@@ -1,11 +1,31 @@
 package com.tripagor.hotels.model;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 public class Hotel {
 	@Id
 	private String id;
+	private int  bookingComId;
+	private String countryCode;
+	private String uif;
+	private String hotelClass;
+	private String currencyCode;
+	private double minrate;
+	private double maxrate;
+	private String preferred;
+	private byte nrRooms;
+	private String longitude;
+	private String latitude;
+	private double publicRanking;
+	private String url;
+	private String imageUrl;
+	
 	private String name;
+	private String address;
+	@Field("city_hotel")
+	private String city;
+	
 
 	public String getId() {
 		return id;
@@ -21,6 +41,22 @@ public class Hotel {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
 	}
 
 }
