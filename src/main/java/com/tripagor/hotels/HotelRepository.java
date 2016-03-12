596402a5b1ead70de12bcea46da32a993416aadc
Jens Laufer <jenslaufer@gmail.com>
2016-03-12 13:16:01 +0100

    added finder

--- src/main/java/com/tripagor/hotels/HotelRepository.java
+++ src/main/java/com/tripagor/hotels/HotelRepository.java
@@ -1,9 +1,34 @@
 package com.tripagor.hotels;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.tripagor.hotels.model.Hotel;
 
+@RepositoryRestResource
 public interface HotelRepository extends MongoRepository<Hotel, String> {
+
+	Hotel findByBookingComId(@Param("bookingId") long id);
+
+	Page<List<Hotel>> findByIsEvaluatedExists(@Param("isExisting") boolean isExisting, Pageable pageable);
+
+	long countByIsEvaluatedExists(@Param("isExisting") boolean isExisting);
+
+	Page<List<Hotel>> findByIsEvaluated(@Param("isEvaluated") boolean isEvaluted, Pageable pageable);
+
+	long countByIsEvaluated(@Param("isEvaluated") boolean isEvaluted);
+
+	Page<List<Hotel>> findByFormattedAddressExistsAndIsMarkerSet(@Param("isExisting") boolean isExisting,
+			@Param("isMarkerSet") boolean isMarkerSet, Pageable pageable);
+
+	long countByFormattedAddressExistsAndIsMarkerSet(@Param("isExisting") boolean isExisting,
+			@Param("isMarkerSet") boolean isMarkerSet);
+	long countBy();
+
 
 }
--- src/main/java/com/tripagor/hotels/model/Hotel.java
+++ src/main/java/com/tripagor/hotels/model/Hotel.java
@@ -1,8 +1,10 @@
 package com.tripagor.hotels.model;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
+@Document
 public class Hotel {
 	@Id
 	private String id;
