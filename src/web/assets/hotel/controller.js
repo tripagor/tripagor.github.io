9ba6593ed9eb8a25be2035b5c4e960497a026a0c
Jens Laufer <jenslaufer@gmail.com>
2016-03-16 22:24:22 +0100

    pointing to api server now

--- src/web/assets/hotel/controller.js
+++ src/web/assets/hotel/controller.js
@@ -4,8 +4,9 @@
 angular.module('hotelCtrls', ['ngResource']).controller('hotelDetailsCtrl', ['$scope', '$resource', '$routeParams', function($scope, $resource, $routeParams) {
     var id = $routeParams.id;
     $scope.item = $resource(
-            'http://localhost:8080/api/hotels/:id')
+            'http://ltripagor.github.io/api/hotels/:id')
         .get({
             id: id
         });
 }]);
+gcloud compute firewall-rules create default-allow-mongo  --allow tcp:27017   --source-ranges 0.0.0.0/0  --target-tags mongodb   --description "Allow mongodb access to all IPs"
