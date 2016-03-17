ccb76d3b926d64bb476c14b1eef6aa9a1d4f18e8
Jens Laufer <jens.laufer@compeople.de>
2016-03-17 13:17:25 +0100

    cleaned code

--- src/web/assets/hotel/controller.js
+++ src/web/assets/hotel/controller.js
@@ -2,10 +2,9 @@
 
 
 angular.module('hotelCtrls', ['ngResource']).controller('hotelDetailsCtrl', ['$scope', '$resource', '$routeParams', function($scope, $resource, $routeParams) {
-    var id = $routeParams.id;
     $scope.item = $resource(
             'http://api.tripagor.com/api/hotels/:id')
         .get({
-            id: id
+            id: $routeParams.id
         });
-}]);
+}])
