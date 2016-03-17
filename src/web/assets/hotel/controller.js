36b38d3c95a2fcbf9a96175e3b89a839bd2805d2
Jens Laufer <jens.laufer@compeople.de>
2016-03-17 11:27:00 +0100

    changed to tripagor api endpoint

--- src/web/assets/hotel/controller.js
+++ src/web/assets/hotel/controller.js
@@ -4,9 +4,8 @@
 angular.module('hotelCtrls', ['ngResource']).controller('hotelDetailsCtrl', ['$scope', '$resource', '$routeParams', function($scope, $resource, $routeParams) {
     var id = $routeParams.id;
     $scope.item = $resource(
-            'http://ltripagor.github.io/api/hotels/:id')
+            'http://api.tripagor.com/api/hotels/:id')
         .get({
             id: id
         });
 }]);
-gcloud compute firewall-rules create default-allow-mongo  --allow tcp:27017   --source-ranges 0.0.0.0/0  --target-tags mongodb   --description "Allow mongodb access to all IPs"
