d41e3b2a4095bdba83cffa0a321e8b838a2156f2
Jens Laufer <jenslaufer@gmail.com>
2016-03-20 14:36:19 +0100

    added interceptor for token

--- src/web/assets/hotel/app.js
+++ src/web/assets/hotel/app.js
@@ -7,4 +7,23 @@ angular.module('hotel', ['ngRoute', 'hotelCtrls']).config(['$routeProvider', fun
     });
 }]).config(['$locationProvider', function($locationProvider) {
     $locationProvider.hashPrefix('!');
+}]).factory('authInterceptor', [
+    "$q", "$window", "$location",
+    function($q, $window, $location) {
+        return {
+            request: function(config) {
+                config.headers = config.headers || {};
+                config.headers.Authorization = 'Bearer e3436a40-eb21-4fbb-ac41-11f11716c85a'; // + session.get('token'); // add your token from your service or whatever
+                return config;
+            },
+            response: function(response) {
+                return response || $q.when(response);
+            },
+            responseError: function(rejection) {
+                // your error handler
+            }
+        };
+    }
+]).config(['$httpProvider', function($httpProvider) {
+    $httpProvider.interceptors.push('authInterceptor');
 }]);
