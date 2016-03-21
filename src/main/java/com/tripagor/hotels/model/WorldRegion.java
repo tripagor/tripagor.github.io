be6750fe212adb73c1e68b333911cdf4ad3f3443
Jens Laufer <jens.laufer@compeople.de>
2016-03-21 10:55:17 +0100

    changed worldregion enum

--- src/main/java/com/tripagor/hotels/model/WorldRegion.java
+++ src/main/java/com/tripagor/hotels/model/WorldRegion.java
@@ -1,9 +1,19 @@
 package com.tripagor.hotels.model;
 
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum WorldRegion {
 	NORTH_AMERICA(1), MIDDLE_AMERICA(2), SOUTH_AMERICA(3), OTHER(4), AFRICA(5), EUROPE(6), MIDDLE_EAST(7), ASIA(
 			8), AUSTRALIA_OCEANIA(9), CARIBBEAN(10), UNKNOWN(0);
 	private int continentId;
+	private static Map<Integer, WorldRegion> map = Stream.of(WorldRegion.values())
+			.collect(Collectors.toMap(s -> s.toValue(), Function.identity()));
 
 	WorldRegion(int continentId) {
 		this.continentId = continentId;
@@ -13,9 +23,20 @@ public enum WorldRegion {
 		return this.continentId;
 	}
 
+	@JsonValue
 	@Override
 	public String toString() {
-		return ""+continentId;
+		return "" + continentId;
+	}
+
+	@JsonCreator
+	public static WorldRegion fromValue(Integer value) {
+		WorldRegion region = map.get(value);
+		if (region == null) {
+			return map.get(0);
+		} else {
+			return region;
+		}
 	}
 
 }
