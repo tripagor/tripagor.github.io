a691033ee336701ff55b9af8edff1e52e69ace96
Jens Laufer <jenslaufer@gmail.com>
2016-03-30 22:09:20 +0200

    changed property mapping

--- src/main/java/com/tripagor/cli/exporter/BookingComExporter.java
+++ src/main/java/com/tripagor/cli/exporter/BookingComExporter.java
@@ -6,7 +6,10 @@ import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
+import org.apache.commons.beanutils.BeanUtils;
 import org.bson.Document;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,6 +17,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.client.RestTemplate;
 
+import com.google.common.base.CaseFormat;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoCollection;
@@ -65,89 +69,24 @@ public class BookingComExporter {
 			for (int i = 0; i < rows.size(); i++) {
 				if (i > 0) {
 					try {
-						// {0=booking_com_id, 1=name, 2=address, 3=zip,
-						// 4=city_hotel, 5=country_code, 6=ufi, 7=class,
-						// 8=currencycode, 9=minrate, 10=maxrate, 11=preferred,
-						// 12=nr_rooms, 13=longitude, 14=latitude,
-						// 15=public_ranking, 16=hotel_url, 17=photo_url,
-						// 18=desc_en, 19=desc_fr, 20=desc_es, 21=desc_de,
-						// 22=desc_nl,
-						// 23=desc_it, 24=desc_pt, 25=desc_ja, 26=desc_zh,
-						// 27=desc_pl, 28=desc_ru, 29=desc_sv, 30=desc_ar,
-						// 31=desc_el, 32=desc_no, 33=city_unique,
-						// 34=city_preferred, 35=continent_id, 36=review_score,
-						// 37=review_nr, 38=null}
 						String[] values = rows.get(i);
 
 						Map<String, Object> valueMap = new HashMap<>();
 						for (int j = 0; j < values.length; j++) {
 							if (values[j] != null) {
-								valueMap.put(propMap.get(j), toObject(getType(propMap.get(j)), values[j]));
+								valueMap.put(CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, propMap.get(j)),
+										toObject(getType(propMap.get(j)), values[j]));
 							}
 						}
 
 						Hotel hotel = new Hotel();
-						if (propMap.get("id") != null) {
-							hotel.setBookingComId(Long.parseLong(propMap.get("id")));
-						}
-						hotel.setName(propMap.get("name"));
-						hotel.setAddress(propMap.get("address"));
-						hotel.setZip(propMap.get("zip"));
-						hotel.setCity(propMap.get("city_hotel"));
-						hotel.setCountryCode(propMap.get("cc1"));
-						hotel.setUfi(propMap.get("ufi"));
-						if (propMap.get("hotelClass") != null) {
-							hotel.setHotelClass(Integer.parseInt(propMap.get("hotelClass")));
-						}
-						hotel.setCurrencycode(propMap.get("currencycode"));
-						if (propMap.get("minrate") != null) {
-							hotel.setMinrate(Double.parseDouble(propMap.get("minrate")));
-						}
-						if (propMap.get("maxrate") != null) {
-							hotel.setMaxrate(Double.parseDouble(propMap.get("maxrate")));
-						}
-						if (propMap.get("nr_rooms") != null) {
-							hotel.setNrRooms(Integer.parseInt(propMap.get("nr_rooms")));
-						}
-						hotel.setLongitude(propMap.get("longitude"));
-						hotel.setLatitude(propMap.get("latitude"));
-						if (propMap.get("public_ranking") != null) {
-							hotel.setPublicRanking(Double.parseDouble(propMap.get("public_ranking")));
-						}
-						hotel.setUrl(propMap.get("hotel_url"));
-						hotel.setImageUrl(propMap.get("photo_url"));
-						hotel.setDescEn(propMap.get("desc_en"));
-						hotel.setDescFr(propMap.get("desc_fr"));
-						hotel.setDescEs(propMap.get("desc_es"));
-						hotel.setDescDe(propMap.get("desc_de"));
-						hotel.setDescNl(propMap.get("desc_nl"));
-						hotel.setDescIt(propMap.get("desc_it"));
-						hotel.setDescPt(propMap.get("desc_pt"));
-						hotel.setDescJa(propMap.get("desc_ja"));
-						hotel.setDescZh(propMap.get("desc_zh"));
-						hotel.setDescPl(propMap.get("desc_pl"));
-						hotel.setDescRu(propMap.get("desc_ru"));
-						hotel.setDescSv(propMap.get("desc_sv"));
-						hotel.setDescAr(propMap.get("desc_ar"));
-						hotel.setDescEl(propMap.get("desc_el"));
-						hotel.setDescNo(propMap.get("desc_no"));
-						hotel.setCityUnique(propMap.get("city_unique"));
-						hotel.setCityPreferred(propMap.get("city_preferred"));
-						if (propMap.get("continent_id") != null) {
-							hotel.setContinentId(Integer.parseInt(propMap.get("continent_id")));
-						}
-						if (propMap.get("review_score") != null) {
-							hotel.setReviewScore(Double.parseDouble(propMap.get("review_score")));
-						}
-						if (propMap.get("review_nr") != null) {
-							hotel.setReviewNr(Integer.parseInt(propMap.get("review_nr")));
-						}
-						if (propMap.get("preferred") != null) {
-							hotel.setPreferredBookingComPartner(Integer.parseInt(propMap.get("preferred")));
-						}
+						BeanUtils.populate(hotel, valueMap);
+						hotel.setCity(valueMap.get("cityHotel").toString());
+						hotel.setUrl(valueMap.get("hotelUrl").toString());
+						hotel.setImageUrl(valueMap.get("photoUrl").toString());
 
 						Hotel loaded = restTemplate.getForObject(
-								restUri.concat("/search/findByBookingComId?bookingId={bookingComId}"), Hotel.class,
+								restUri.concat("/search/findByBookingComId?bookingComId={bookingComId}"), Hotel.class,
 								hotel.getBookingComId());
 						if (loaded == null) {
 							restTemplate.postForObject(restUri, hotel, Hotel.class);
