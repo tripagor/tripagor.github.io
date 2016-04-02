36721571a8e01dcbb753b22ccf05b62c0fd4b5be
Jens Laufer <jenslaufer@gmail.com>
2016-04-02 15:13:47 +0200

    added place marker logic

--- src/main/java/com/tripagor/cli/exporter/PlaceMarker.java
+++ src/main/java/com/tripagor/cli/exporter/PlaceMarker.java
@@ -2,16 +2,28 @@ package com.tripagor.cli.exporter;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
+import java.util.Collection;
 
 import org.bson.Document;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.hateoas.PagedResources;
+import org.springframework.hateoas.hal.Jackson2HalModule;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mongodb.Block;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.tripagor.cli.service.PlaceAddApi;
+import com.tripagor.hotels.model.Hotel;
 import com.tripagor.model.Location;
 import com.tripagor.model.PlaceAddRequest;
 import com.tripagor.model.PlaceAddResponse;
@@ -21,10 +33,23 @@ public class PlaceMarker {
 
 	private int numberOfPlacesToAdd;
 	private String appendStr = "";
+	private RestTemplate hateoasRestTemplate;
+	private int pageSize = 40;
+	private RestTemplate patchRestTemplate;
 	private RestTemplateFactory restTemplateFactory;
 
 	public PlaceMarker() {
-		this.restTemplateFactory = new RestTemplateFactory();
+		 restTemplateFactory = new RestTemplateFactory();
+
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		mapper.registerModule(new Jackson2HalModule());
+
+		MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+		converter.setSupportedMediaTypes(MediaType.parseMediaTypes("application/hal+json"));
+		converter.setObjectMapper(mapper);
+
+		hateoasRestTemplate = restTemplateFactory.get(converter);
 	}
 
 	public void doMark(String uri, String collectionName, String key) {
@@ -78,13 +103,57 @@ public class PlaceMarker {
 		}
 
 	}
-	
 
-	public void doMark(String restUri, String clientId, String clientSecret, String key) {
-		// TODO Auto-generated method stub
+	public void doMark(String host, String clientId, String clientSecret, String key) {
+		PlaceAddApi placeAddApi = new PlaceAddApi(key);
+
+		patchRestTemplate = restTemplateFactory.get(host,clientId, clientSecret);
+		int currentPage = 0;
+		long totalPages = 1;
+
 		
-	}
+		while (currentPage < totalPages) {
+			PagedResources<Hotel> pagedResources = hateoasRestTemplate.exchange(
+					host.concat("hotels/search/findByIsEvaluatedAndIsMarkerSetAndIsMarkerApprovedAndFormattedAddressExists?page={page}&size={size}&isEvaluated=true&isMarkerSet=false&isMarkerApproved=false&isFormattedAddressExisting=true"),
+					HttpMethod.GET, null, new ParameterizedTypeReference<PagedResources<Hotel>>() {
+					}, currentPage++, pageSize).getBody();
+			totalPages = pagedResources.getMetadata().getTotalPages();
+			Collection<Hotel> hotels = pagedResources.getContent();
 
+			for (Hotel hotel : hotels) {
+				try {
+					System.out.println(hotel.getBookingComId() + " " + hotel.getName());
+					PlaceAddRequest place = new PlaceAddRequest();
+
+					place.setName(hotel.getName());
+					place.setAddress(hotel.getFormattedAddress());
+					Location location = new Location();
+					location.setLat(new BigDecimal(hotel.getLatitude()).doubleValue());
+					location.setLng(new BigDecimal(hotel.getLongitude()).doubleValue());
+					place.setLocation(location);
+					place.setAccuracy(30);
+					place.setWebsite(hotel.getUrl() + appendStr);
+					place.setTypes(Arrays.asList(new String[] { "lodging" }));
+
+					PlaceAddResponse response = new PlaceAddResponse();// placeAddApi.add(place);
+					response.setStatus("OK");
+					response.setPlaceId("AAA99292");
+					if ("OK".equals(response.getStatus())) {
+						hotel.setPlaceId(response.getPlaceId());
+						hotel.setIsMarkerSet(true);
+
+						HttpHeaders headers = new HttpHeaders();
+						headers.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON.toString());
+						HttpEntity<Hotel> requestEntity = new HttpEntity<>(hotel, headers);
+						patchRestTemplate.exchange(host.concat("/hotels/{id}"), HttpMethod.PATCH, requestEntity,
+								String.class, hotel.getId());
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 
 	public void setNumberOfPlacesToAdd(int numberOfPlacesToAdd) {
 		this.numberOfPlacesToAdd = numberOfPlacesToAdd;
