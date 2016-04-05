b5e5c2347a9a180d914e08164f7133448b67ac09
Jens Laufer <jenslaufer@gmail.com>
2016-04-05 15:00:14 +0200

    fixed code

--- src/main/java/com/tripagor/cli/service/PlaceAddApiSeleniumImpl.java
+++ src/main/java/com/tripagor/cli/service/PlaceAddApiSeleniumImpl.java
@@ -35,25 +35,22 @@ public class PlaceAddApiSeleniumImpl implements PlaceAddApi {
 			driver.findElement(By.id("signIn")).click();
 			driver.findElement(By.id("searchboxinput")).clear();
 			driver.findElement(By.id("searchboxinput")).sendKeys(place.getAddress());
+		    driver.findElement(By.cssSelector("button.searchbox-searchbutton")).click();
 			driver.findElement(By.cssSelector("button.widget-pane-term-link.widget-pane-link")).click();
 			driver.findElement(By.cssSelector("input.rap-text-input")).clear();
 			driver.findElement(By.cssSelector("input.rap-text-input")).sendKeys(place.getName());
-			driver.findElement(By.id(":3h")).click();
-			driver.findElement(By.cssSelector(
-					"div.add-a-place-card-field.add-a-place-card-field-type-category > div.issue-card-field-value-container > div.rap-text-input-container > input.rap-text-input"))
-					.clear();
-			driver.findElement(By.cssSelector(
-					"div.add-a-place-card-field.add-a-place-card-field-type-category > div.issue-card-field-value-container > div.rap-text-input-container > input.rap-text-input"))
-					.sendKeys("Unterkünfte");
 			driver.findElement(By.cssSelector(
 					"div.add-a-place-card-field.add-a-place-card-field-type-website > div.issue-card-field-value-container > div.rap-text-input-container > input.rap-text-input"))
 					.clear();
 			driver.findElement(By.cssSelector(
 					"div.add-a-place-card-field.add-a-place-card-field-type-website > div.issue-card-field-value-container > div.rap-text-input-container > input.rap-text-input"))
 					.sendKeys(place.getWebsite());
+
+		    driver.findElement(By.id(":7n")).click();
 			driver.findElement(By.xpath("//div[@id='rap-card']/div/div/div[2]/div[3]/button")).click();
 
 			placeAddResponse.setPlaceId("NO_PLACE_ID_ADDED_WITH_SELENIUM_" + username);
+			//placeAddResponse.setStatus("OK");
 		} catch (Exception e) {
 			System.err.println("error " + e);
 			placeAddResponse.setStatus("FAILED");
